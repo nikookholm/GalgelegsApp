94de56cc0239f6fcac4ae623434332f605dae85a
eggman1979 <kim.d.rasmussen@live.dk>
2016-01-05 13:44:45 +0100

    Tillføjet setOnclickListener til spilBtn

--- app/src/main/java/s112011/galgelegsapp/MainManuActivity.java
+++ app/src/main/java/s112011/galgelegsapp/MainManuActivity.java
@@ -1,13 +1,29 @@
 package s112011.galgelegsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainManuActivity extends AppCompatActivity {
+public class MainManuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button spilBtn, highScoresBtn, indstillingBtn, feedbackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_manu);
+
+        spilBtn = (Button) findViewById(R.id.spilKnap);
+        spilBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == spilBtn){
+            Intent visLevels = new Intent(this, Level_Activity.class);
+            startActivity(visLevels);
+            }
     }
 }

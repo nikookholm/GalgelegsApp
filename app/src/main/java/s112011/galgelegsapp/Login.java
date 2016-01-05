eff826e6c0d9920bf48de125d58822143a874f00
onepiece <favad_saidi13@hotmail.com>
2016-01-05 13:37:35 +0100

    Tilføjet intent i login så der bliver vist frem til hovedmenuen når der bliver trykket på en button.
    Tilføjet Level_Activity

--- app/src/main/java/s112011/galgelegsapp/Login.java
+++ app/src/main/java/s112011/galgelegsapp/Login.java
@@ -1,13 +1,30 @@
 package s112011.galgelegsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private Button offlineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        offlineButton = (Button) findViewById(R.id.offlineButton);
+        offlineButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        if(v==offlineButton){
+            Intent goToMainMenu = new Intent(this, MainManuActivity.class);
+            startActivity(goToMainMenu);
+        }
     }
 }

b00255fd1b20a89b555af803881e0275995d9c1a
onepiece <favad_saidi13@hotmail.com>
2016-01-14 13:35:08 +0100

    Tilføjet Nye metoder til Google login bl.a. til at hente data når man logger ind

--- app/src/main/java/s112011/galgelegsapp/Login.java
+++ app/src/main/java/s112011/galgelegsapp/Login.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -24,9 +25,13 @@ public class Login extends AppCompatActivity{
     private Button offlineButton;
     GoogleSignInOptions googleOp;
     GoogleApiClient googleApiClient;
+    GoogleSignInAccount googleAccount;
+    GoogleSignInResult googleResult;
     private static final int RC_SIGN_IN = 0;
     SignInButton sign_in_button;
     public Activity a = this;
+    private static final String TAG = "Login";
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
@@ -55,8 +60,45 @@ public class Login extends AppCompatActivity{
 
     // Log ind verificering
     private void signIn() {
+
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        googleResult.getSignInAccount();
+        googleAccount.getDisplayName();
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isSignedIn){
+        if(isSignedIn){
+            sign_in_button.setVisibility(View.GONE);
+            sign_in_button.setVisibility(View.VISIBLE);
+        }else {
+            sign_in_button.setVisibility(View.GONE);
+            sign_in_button.setVisibility(View.VISIBLE);
+        }
     }
     // Opretter en ny inner-class af ConFailListener
     private class ConFailListener implements GoogleApiClient.OnConnectionFailedListener{
